db5f7c2b7fcb0c55d0e0b335090431a0a22cff82
huhuhang <huhuhang@gmail.com>
2019-07-08 16:58:39 +0800

    Update

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ import requests
 import pandas as pd
 
 df = pd.read_csv("mushrooms.csv")  # 读取原数据集
-sample_data = df.sample(1).to_json()  # 从原数据中随机取 1 条用于测试推理，并处理成 JSON 类型
+sample_data = df.sample(1).to_json(orient='records')  # 从原数据中随机取 1 条用于测试推理，并处理成 JSON 类型
 requests.post(url="https://mushrooms-prediction.herokuapp.com", json=sample_data).content  # 建立 POST 请求，并发送数据请求
 ```
--- run.py
+++ run.py
@@ -1,6 +1,19 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
+
+
+def first():
+    df = pd.read_csv("mushrooms.csv")  # 读取数据
+    X = pd.get_dummies(df.iloc[:, 1:])  # 读取特征并独热编码
+    y = df['class']  # 目标值
+
+    model = MLPClassifier()  # 人工神经网络
+    model.fit(X, y)  # 训练模型
+    joblib.dump(model, "mushrooms.pkl")  # 保存模型
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -10,10 +23,9 @@ def index():
 
 @app.route("/", methods=["POST"])  # 请求方法为 POST
 def inference():
-
     df = pd.read_csv("mushrooms.csv")  # 读取数据
     X = pd.get_dummies(df.iloc[:, 1:])  # 读取特征并独热编码
-    query_df = pd.DataFrame([request.json])  # 将 JSON 变为 DataFrame
+    query_df = pd.DataFrame(request.json)  # 将 JSON 变为 DataFrame
     query = pd.get_dummies(query_df).reindex(
         columns=X.columns, fill_value=0)  # 将请求数据 DataFrame 处理成独热编码样式
 
@@ -29,8 +41,9 @@ def inference():
         status = False
 
     # 返回推理结果
-    return jsonify({"prediction": list(preds), "predict_proba": round(float(preds_proba), 3), "partial_fit": status})
+    return jsonify({"prediction": list(preds), "predict_proba": round(float(preds_proba), 5), "partial_fit": status})
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    first()  # 首次完整训练模型
+    app.run()
